bd01efc1da187d8347a9b149b46a6a82f3ad4e6d
knight <624867243@qq.com>
2015-11-07 02:16:40 +0800

    cccccc

--- shell批量密码验证执行命令/expect版/版本2-传入三个参数/ssh.sh
+++ shell批量密码验证执行命令/expect版/版本2-传入三个参数/ssh.sh
@@ -5,5 +5,6 @@ j=`awk -v I="$i" '{if(I==$1)print $2}' passwd.txt`
 export cmdfile="config/commands.txt"
 
 #expect /root/shell/login.exp $i $j
-expect /root/shell_scripts/bb/login.exp $i $j $cmdfile 
+#expect /root/shell_scripts/bb/login.exp $i $j $cmdfile 
+expect login.exp $i $j $cmdfile 
 done
